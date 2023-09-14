4e13003e38077eac9bf580e8cd5fd9e71be1ecc1
bucketjade <jade.shuyao.zhang@gmail.com>
2023-09-14 00:12:36 -0700

    halfway through making components. to do: fix favicon on local and parse object in CourseList.

---
+++ src/components/Banner.jsx
@@ -0,0 +1,5 @@
+const Banner = ({title}) => (
+    <p>Hello World, I am Banner! I got: { title }</p>
+);
+
+export default Banner;
---
+++ src/components/CourseList.jsx
@@ -0,0 +1,5 @@
+const CourseList = ({courses}) => (
+    <p>Hello World, I am CourseList. Next up is to figure out how to print the list of courses in the format.</p>
+);
+
+export default CourseList;
