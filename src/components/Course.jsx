74692b250ff026874a95b43c37927be3ad183791
bucketjade <jade.shuyao.zhang@gmail.com>
2023-10-12 21:55:32 -0500

    added conflict message

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -6,6 +6,7 @@ const Course = ({id, course, selectedCards, toggleSelectedCards, conflicting}) =
         {/* Note: we assume you will never get to a state where two conflicting classes are selected */}
         <div className={`${selectedCards.includes(id) ? 'course-selected' : ''} ${conflicting ? 'course-conflict' : ''}`}>
             <div className="card-body">
+                {conflicting && ! selectedCards.includes(id) ? <h5 style={{ color: 'white' }}>Course Conflict!</h5> : null}
                 <h5 className="card-title">{course.term} CS {course.number}</h5>
                 <p className="card-text">{course.title}</p>
             </div>
