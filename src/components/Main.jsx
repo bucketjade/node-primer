036a217586b96932bdff2eac004bf370d6154e0d
bucketjade <jade.shuyao.zhang@gmail.com>
2023-10-16 23:53:20 -0500

    add a firebase database

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,15 @@
-import { useJsonQuery } from '../utilities/fetch';
+// import { useJsonQuery } from '../utilities/fetch';
+import { useDbData } from "../utilities/firebase";
 import Banner from './Banner';
 import Dispatcher from './Dispatcher';
 
 const Main = () => {
-    const [schedule, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+    // const [schedule, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
   
+    const [schedule, error] = useDbData("/");
     if (error) return <h1>Error loading user data: {`${error}`}</h1>;
-    if (isLoading) return <h1>Loading user data...</h1>;
-    if (!schedule) return <h1>No user data found</h1>;
+    if (schedule === undefined) return <h1>Loading user data...</h1>;
+    if (!schedule) return <h1>No data found</h1>;
   
     return <div>
         <Banner title={schedule.title} />
---
+++ src/utilities/firebase.js
@@ -0,0 +1,53 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, onValue, ref, update} from 'firebase/database';
+import { useCallback, useEffect, useState } from 'react';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_1T8hdQtbppxdX-fLCLQfqPa9T1EXY_M",
+  authDomain: "node-primer.firebaseapp.com",
+  databaseURL: "https://node-primer-default-rtdb.firebaseio.com",
+  projectId: "node-primer",
+  storageBucket: "node-primer.appspot.com",
+  messagingSenderId: "665743648563",
+  appId: "1:665743648563:web:6d190e82ee0e67e3f50046"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useDbData = (path) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
+
+  useEffect(() => (
+    onValue(ref(database, path), (snapshot) => {
+     setData( snapshot.val() );
+    }, (error) => {
+      setError(error);
+    })
+  ), [ path ]);
+
+  return [ data, error ];
+};
+
+const makeResult = (error) => {
+  const timestamp = Date.now();
+  const message = error?.message || `Updated: ${new Date(timestamp).toLocaleString()}`;
+  return { timestamp, error, message };
+};
+
+export const useDbUpdate = (path) => {
+  const [result, setResult] = useState();
+  const updateData = useCallback((value) => {
+    update(ref(database, path), value)
+    .then(() => setResult(makeResult()))
+    .catch((error) => setResult(makeResult(error)))
+  }, [database, path]);
+
+  return [updateData, result];
+};
