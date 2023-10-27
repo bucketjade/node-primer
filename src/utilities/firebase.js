92f8c363771528203c58100f37612dd24a6f9101
bucketjade <jade.shuyao.zhang@gmail.com>
2023-10-27 14:03:54 -0500

    campuswire question 77

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -23,27 +23,27 @@ const firebase = initializeApp(firebaseConfig);
 const database = getDatabase(firebase);
 const auth = getAuth(firebase);
 
-// if (process.env.REACT_APP_EMULATE) {
-//   connectAuthEmulator(auth, "http://127.0.0.1:9099");
-//   connectDatabaseEmulator(database, "127.0.0.1", 9000);
-
-//   signInWithCredential(auth, GoogleAuthProvider.credential(
-//     '{"sub": "eifJU2mLQx89KlSCOGRpB9t8Bcja", "email": "tester314159@gmail.com", "displayName":"Test User", "email_verified": true}'
-//   ));
-// }
-
-
-if (!window.EMULATION && import.meta.env.NODE_ENV !== 'production') {
+if (process.env.REACT_APP_EMULATE) {
   connectAuthEmulator(auth, "http://127.0.0.1:9099");
   connectDatabaseEmulator(database, "127.0.0.1", 9000);
 
   signInWithCredential(auth, GoogleAuthProvider.credential(
     '{"sub": "eifJU2mLQx89KlSCOGRpB9t8Bcja", "email": "tester314159@gmail.com", "displayName":"Test User", "email_verified": true}'
   ));
-  
-  // set flag to avoid connecting twice, e.g., because of an editor hot-reload
-  window.EMULATION = true;
 }
+
+
+// if (!window.EMULATION && import.meta.env.NODE_ENV !== 'production') {
+//   connectAuthEmulator(auth, "http://127.0.0.1:9099");
+//   connectDatabaseEmulator(database, "127.0.0.1", 9000);
+
+//   signInWithCredential(auth, GoogleAuthProvider.credential(
+//     '{"sub": "eifJU2mLQx89KlSCOGRpB9t8Bcja", "email": "tester314159@gmail.com", "displayName":"Test User", "email_verified": true}'
+//   ));
+  
+//   // set flag to avoid connecting twice, e.g., because of an editor hot-reload
+//   window.EMULATION = true;
+// }
 
 // Database functions
 export const useDbData = (path) => {
